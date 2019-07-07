1d1ce11dfc1571469b5ba42b1ad6c1611b6ba6ac
10fu3 <31952653+10fu3@users.noreply.github.com>
2019-07-07 13:10:00 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send(getIP(req))
+})
+
+var getIP = function (req) {
+  if (req.headers['x-forwarded-for']) {
+    return req.headers['x-forwarded-for'];
+  }
+  if (req.connection && req.connection.remoteAddress) {
+    return req.connection.remoteAddress;
+  }
+  if (req.connection.socket && req.connection.socket.remoteAddress) {
+    return req.connection.socket.remoteAddress;
+  }
+  if (req.socket && req.socket.remoteAddress) {
+    return req.socket.remoteAddress;
+  }
+  return '0.0.0.0';
+};
