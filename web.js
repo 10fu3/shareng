e9494349b0d3f27d670ec914a98ebbf11cfdd904
10fu3 <31952653+10fu3@users.noreply.github.com>
2019-07-07 14:20:27 +0900

    Add files via upload

--- web.js
+++ web.js
@@ -2,8 +2,40 @@ var express = require('express');
 
 var app = express.createServer();
 
-app.get('/users/:UID/targetID/:target/', function (req, res) {
-  res.send(req.params['UID']+req.params['target'])
+var list = Array('SharedNGID');
+var users = Map()
+
+class User{
+  constructor(uid) {
+    this.uid = uid;
+    this.apendedID = Array();
+  }
+  
+  getID(){
+    return this.uid;
+  }
+  addTarget(target){
+    if(!this.apendedID.includes(target)){
+      this.apendedID.push(target);
+      list.push(target)
+    }
+  }
+}
+
+app.get('/uid/:uid/targetid/:target/', function (req, res) {
+  let uid = req.params['uid'];
+  let target = req.params['target'];
+  if(!users.has(uid)){
+    var u = new User(uid);
+    users.set(uid,new User(uid));
+  }
+  var u = users.get(uid);
+  u.addTarget(target);
+  res.send(list)
+})
+
+app.get('/',function(req,res){
+  res.send(list)
 })
 
 var port = process.env.PORT || 3000;
