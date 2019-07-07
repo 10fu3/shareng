c4812efa97a07dbcd16cce534896aecc295afa87
10fu3 <31952653+10fu3@users.noreply.github.com>
2019-07-07 13:16:38 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  var ip = req.connection.remoteAddress;
-  res.send(ip)
+  res.send('hello')
 })
 
